e7a45513d951d9577228acd6bf886855d55e983c
Patrick Hirling <patrick.hirling@epfl.ch>
2022-06-01 11:12:55 +0200

    Updates & film script

--- mestel/film.py
+++ mestel/film.py
@@ -1,10 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.colors import LogNorm, PowerNorm
+from matplotlib.colors import Normalize, LogNorm, PowerNorm
+from matplotlib.animation import FuncAnimation
 import argparse
 import h5py
 
-parser = argparse.ArgumentParser(description="Plot 2d surface density")
+parser = argparse.ArgumentParser(description="Make film of surface density of snapshots, in order")
 
 # Snapshot Parameters
 parser.add_argument("files",nargs='+',help="snapshot files to be imaged")
@@ -21,17 +22,16 @@ parser.add_argument("--polar",action='store_true',help="Pot a r-phi histogram ra
 parser.add_argument("-cmap",type=str,default='YlGnBu_r',help="Colormap (try YlGnBu_r, Magma !)")
 parser.add_argument("-norm",type=str,default='log',help='Color Norm to use')
 parser.add_argument("-gamma",type=float,help="Exponent of power law norm (use -norm power)")
-parser.add_argument("-cmin",type=float,default=-1,help=
-                    """Minimum Physical value in the Histogram (is automatically set to >0 for log norm).
-                    This effectively sets the contrast of the images. Set to negative to use min value across snapshots""")
+parser.add_argument("-cmin",type=float,default=0,help=
+                    """Minimum Physical value in the Histogram.
+                    This effectively sets the contrast of the images.""")
+parser.add_argument("-cmax",type=float,default=300,help="Maximum physical value in the Histogram.")
 
 # Figure Parameterrs
 parser.add_argument("--notex",action='store_true')
-parser.add_argument("-nrows",type=int,default=2,help="Number of rows in figure")
-parser.add_argument("-ncols",type=int,default=3,help="Number of columns in figure")
-parser.add_argument("-figheight",type=float,default=8,help="Height of figure in inches")
-parser.add_argument("-figwidth",type=float,default=12,help="Width of figure in inches")
-parser.add_argument("--savefig",action='store_true',help="Save figure as .eps")
+parser.add_argument("-figheight",type=float,default=10,help="Height of figure in inches")
+parser.add_argument("-figwidth",type=float,default=10,help="Width of figure in inches")
+parser.add_argument("--savefig",action='store_true',help="Save film as .mp4")
 
 # Corotation Parameters
 parser.add_argument("-rcr",type=float,default=0.0,
@@ -43,10 +43,11 @@ parser.add_argument("-vcr",type=float,default=200,
 parser.add_argument("--set_origin",action='store_true',
     help="Set the origin of the figure to the corotating origin (default: no)")
 
+# Film Parameters
+parser.add_argument("-fps",type=float,default=24,help="Frames per second")
+
 args = parser.parse_args()
 fnames = args.files
-if len(fnames) > args.nrows*args.ncols:
-    raise ValueError("Too many snapshots to fit in figure")
 
 # Convenience
 lim = args.lim
@@ -54,19 +55,18 @@ active_id_start = int(args.aid)
 
 # Configure histogram color norm
 if args.norm == 'power':
-    norm = PowerNorm(gamma=args.gamma)
+    norm = PowerNorm(gamma=args.gamma,vmin=args.cmin,vmax=args.cmax)
 elif args.norm == 'log':
     # The color range is later automatically adjusted to >0 if log norm
-    norm = LogNorm(clip=True)
+    norm = LogNorm(clip=True,vmin=args.cmin,vmax=args.cmax)
 elif args.norm == 'linear':
-    norm = None # Equivalent to linear
+    norm = Normalize(vmin=args.cmin,vmax=args.cmax) # Equivalent to linear
 else:
     raise NameError("Unknown color norm: " + str(args.norm))
 
 # Configure Pyplot
-if not args.notex: plt.rcParams.update({"text.usetex": True,'font.size':1.2*args.figheight,'font.family': 'serif'})
+if not args.notex: plt.rcParams.update({"text.usetex": True,'font.size':2*args.figheight,'font.family': 'serif'})
 else: plt.rcParams.update({'font.size':15})
-
 
 # Set up corotation
 corot = args.rcr > 0
@@ -81,14 +81,12 @@ if corot:
         y = np.sin(omega*t)*X + np.cos(omega*t)*Y - y0
         return x,y
 
-cmax = 0. # The max of the color range is computed across all snapshots
-cmin = 0.
-fig, ax = plt.subplots(args.nrows,args.ncols,figsize=(args.figwidth,args.figheight))
-ax = ax.flatten()
-ims = []
-for i,fname in enumerate(fnames):
+# Initialize plot
+fig, ax = plt.subplots(figsize=(args.figwidth,args.figheight))
+
+def update(i):
     # Extract data from snapshot
-    f = h5py.File(fname, "r")
+    f = h5py.File(fnames[i], "r")
     pos = np.array(f["DMParticles"]["Coordinates"]) - args.shift
     IDs = np.array(f["DMParticles"]["ParticleIDs"])
     pos = pos[IDs>=active_id_start]
@@ -104,37 +102,29 @@ for i,fname in enumerate(fnames):
     # Make image
     if not args.polar:
         data = np.histogram2d(x,y,bins=args.nbins,range=[[-lim, lim], [-lim, lim]])[0]
-        ims.append(ax[i].imshow(data.T, interpolation = args.interp, norm=norm,
-                    extent=(-lim,lim,-lim,lim),cmap=args.cmap))
-        ax[i].set_xlim(-lim,lim)
-        ax[i].set_ylim(-lim,lim)
-        ax[i].set_aspect('equal')
-        ax[i].set_xlabel('x [kpc]')
-        ax[i].set_ylabel('y [kpc]')
+        ax.imshow(data.T, interpolation = args.interp, norm=norm,
+                    extent=(-lim,lim,-lim,lim),cmap=args.cmap)
+        ax.set_xlim(-lim,lim)
+        ax.set_ylim(-lim,lim)
+        ax.set_aspect('equal')
+        ax.set_xlabel('x [kpc]')
+        ax.set_ylabel('y [kpc]')
     else:
         r = np.sqrt(x**2 + y**2)
         phi = np.arctan2(y,x)
-        data,xe,ye,im = ax[i].hist2d(r,phi,range=[[0,lim],[-np.pi,np.pi]],
-            bins=args.nbins,cmap=args.cmap,norm=norm)
-        ims.append(im)
-        ax[i].set_box_aspect(1)
-        ax[i].set_xlabel('$r$ [kpc]')
-        ax[i].set_ylabel('$\phi$ [rad]')
+        ax.hist2d(r,phi,range=[[0,lim],[-np.pi,np.pi]],
+                    bins=args.nbins,cmap=args.cmap,norm=norm)
+        ax.set_box_aspect(1)
+        ax.set_xlabel('$r$ [kpc]')
+        ax.set_ylabel('$\phi$ [rad]')
+ 
+    ax.set_title(r'$t=$ ' + "{:.2f}".format(t_myr) + ' Myr')
 
-    # Find global cmin, cmax
-    cmin = min(cmin,np.amin(data))
-    cmax = max(cmax,np.amax(data)) 
-    ax[i].set_title(r'$t=$ ' + "{:.2f}".format(t_myr) + ' Myr',fontsize=args.figwidth)
-    
-# Set limits of color range to global min/max across snapshots
-if args.cmin > 0.0:
-    cmin = args.cmin
-print((cmin,cmax))
-if cmin <= 0.0 and args.norm == 'log': cmin = 1. # Can be improved...
-for im in ims:
-    im.set(clim=(cmin,cmax))
+# Animate
+ani = FuncAnimation(fig, update,frames=range(len(fnames)),interval=100, repeat=1)
 
 # Show & Save figure
-plt.show()
 if args.savefig:
-    fig.savefig('densities_mult.eps',bbox_inches='tight')
+    ani.save('film.mp4',writer='ffmpeg',fps=args.fps,bitrate=-1)
+else:
+    plt.show()
